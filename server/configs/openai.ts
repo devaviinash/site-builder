01b3897acf166bfd90d6107a3525148121677025
devaviinash <avinashchavan127@gmail.com>
2026-03-23 23:47:21 +0530

    initial commit

---
+++ server/configs/openai.ts
@@ -0,0 +1,8 @@
+import OpenAI from 'openai';
+
+const openai = new OpenAI({
+  baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/",
+  apiKey: process.env.AI_API_KEY,
+});
+
+export default openai
