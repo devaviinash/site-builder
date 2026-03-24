41d025a000758b1aaf024c11da28407801e61701
devaviinash <avinashchavan127@gmail.com>
2026-03-24 08:49:44 +0530

    disable sign-ups

--- client/src/pages/auth/AuthPage.tsx
+++ client/src/pages/auth/AuthPage.tsx
@@ -5,7 +5,13 @@ export default function AuthPage() {
   const { pathname } = useParams()
 
   return (
-    <main className="p-6 flex flex-col justify-center items-center h-[80vh]">
+    <main className="p-6 flex flex-col justify-center items-center min-h-[80vh] gap-6 text-center">
+      <div className="bg-orange-500/10 border border-orange-500/50 text-orange-200 px-6 py-4 rounded-lg max-w-md">
+        <h3 className="font-semibold text-lg mb-1">Sign-ups Currently Disabled</h3>
+        <p className="text-sm opacity-90">
+          Due to heavy traffic and to manage server capacity, we are temporarily disabling new sign-ups. You can still login if you already have an account.
+        </p>
+      </div>
       <AuthView pathname={pathname} classNames={{base: 'bg-black/10 ring ring-indigo-900'}}/>
     </main>
   )
